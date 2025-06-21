9458b034fe793b610c1395bb13b251dbddcddc13
Saurabh <sourabhsingh22112003@gmail.com>
2025-06-21 20:32:16 +0530

    make responsive searching part

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -33,7 +33,7 @@ const Search = () => {
    
 
     return (
-        <div className='w-full min-w-[380px] lg:min-w-[420px] h-11 lg:h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-50 group focus-within:border-blue-300'>
+        <div className='w-full lg:min-w-[420px] h-11 lg:h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-50 group focus-within:border-blue-300'>
 
             <div>
                 {
